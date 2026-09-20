8c9b1934e9f6358be4d43d663f89e6c9db505a2e
Hitesh Shah <hiteshhighest122@gmail.com>
2026-09-20 19:04:10 +0545

    Prepare Django project for Vercel

--- core/views.py
+++ core/views.py
@@ -1,36 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Newsletter, TeamMember, ContactMessage
 
 def index_view(request):
-    newsletters = Newsletter.objects.all().order_by('-issue_date')[:3]
-    return render(request, 'core/index.html', {'newsletters': newsletters})
+    return render(request, 'core/index.html')
 
 def gallery_view(request):
     return render(request, 'core/gallery.html')
 
 def team_view(request):
-    mentors_advisors = TeamMember.objects.filter(role__in=['Mentor', 'Advisor'])
-    executives = TeamMember.objects.filter(role='Executive')
-    context = {
-        'mentors_advisors': mentors_advisors,
-        'executives': executives
-    }
-    return render(request, 'core/team.html', context)
+    return render(request, 'core/team.html')
 
 def magazine_view(request):
-    magazine = Newsletter.objects.all().order_by('-issue_date')
-    return render(request, 'core/magazine.html', {'magazines': magazine})
+    return render(request, 'core/magazine.html')
 
 def contact_view(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        
-        ContactMessage.objects.create(name=name, email=email, subject=subject, message=message)
-        messages.success(request, 'Your message has been sent successfully to the SEDS-Loyola team!')
-        return redirect('contact')
-        
     return render(request, 'core/contact.html')
--- seds_loyola_project/settings.py
+++ seds_loyola_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%zw^nvj(t!)xr14c=k86h=#3@u20%apq3rh^=$ie7ubq&&ftnc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -112,7 +113,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
